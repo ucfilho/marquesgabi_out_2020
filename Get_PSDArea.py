a9b7589e1c1ace675318ea805e6ce15afea01521
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-10-16 10:58:07 -0300

    Update Get_PSDArea.py

--- Get_PSDArea.py
+++ Get_PSDArea.py
@@ -1,23 +1,36 @@
+import cv2
+import numpy as np
+import pandas as pd
+
 def PSDArea(df_size):
   Size=28
-  qual_img=40
-  L = Width[qual_img]
-  data=np.array(df_size.drop('Width',axis=1).iloc[qual_img]).reshape(Size,Size)
-  img = Image.fromarray(data.astype('uint8'), mode='L')
-  img=np.float32(img)
-  img28=cv2.resize(img,(Size,Size), interpolation = cv2.INTER_AREA)
-  Foto=np.array(img28).reshape(28,28)
-  plt.imshow(Foto, cmap = "gray")
+  Nx, Ny = df_size.shape
+  Area_todas = []
+  for qual_img in range(Nx):
+    L = Width[qual_img]
+    data=np.array(df_size.drop('Width',axis=1).iloc[qual_img]).reshape(Size,Size)
+    img = Image.fromarray(data.astype('uint8'), mode='L')
+    img=np.float32(img)
+    
+    mean_value = np.mean(img)
+    img_new = img.copy()
+    
+    img28=cv2.resize(img,(Size,Size), interpolation = cv2.INTER_AREA)
+    Foto=np.array(img28).reshape(28,28)
+    
+
+    for i in range(28):
+      for j in range(28):
+        if img[i,j] < mean_value:
+          img_new[i,j] = 255
+        else:
+          img_new[i,j] = 0
+          
+
+    Area = np.sum(img_new) / (255.0 * 28 * 28)* L*L
+    Area_todas.append(Area)
+
+
+    return Area_todas
+
   
-  for i in range(28):
-    for j in range(28):
-      if img[i,j] < mean_value:
-        img_new[i,j] = 255
-      else:
-        img_new[i,j] = 0
-
-  img28=cv2.resize(img_new,(Size,Size), interpolation = cv2.INTER_AREA)
-  Foto=np.array(img28).reshape(28,28)
-  plt.imshow(Foto, cmap = "gray")
-  return df_areas
-
